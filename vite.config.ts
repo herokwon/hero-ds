2dac8b482c787af3e9e353e6f33c04f25d7ebb56
HeroKwon <contact@herokwon.dev>
2025-06-01 03:47:24 +0900

    chore: Set up testing configuration (feat. `Testing-Library`, `Vitest`) (#9)
    * chore: 테스팅 도구(`Testing-Library`, `Vitest`) 의존성 추가
    
    * chore: 초기 테스팅(`Testing-Library`, `Vitest`) 환경설정 구성

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,23 @@
+/// <reference types="vitest/config" />
 import { defineConfig } from 'vite';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
 import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react(), tailwindcss(), tsconfigPaths()],
+  test: {
+    globals: true,
+    pool: 'threads',
+    environment: 'jsdom',
+    setupFiles: 'vitest.setup',
+    include: ['src/**/*.(spec|test).ts?(x)'],
+    coverage: {
+      enabled: true,
+      provider: 'v8',
+      include: ['src'],
+      exclude: ['src/**/!(*.spec|*.test).ts?(x)'],
+    },
+  },
 });
