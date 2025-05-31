ee305bb3482f974fcc16126f9938c517c3642880
HeroKwon <contact@herokwon.dev>
2025-06-01 02:35:39 +0900

    chore: Set up code quality configuration (#5)
    * chore: `Prettier` 및 플러그인 의존성 추가
    
    * chore: `Prettier` 초기 환경설정 구성
    
    * style: `Prettier` 코드 스타일 적용
    
    * chore: `ESLint` 및 확장 의존성 추가
    
    * chore: `ESLint` 초기 환경설정 구성
    
    * chore: `Prettier` 설정 범위 마크다운 파일 제외

--- eslint.config.js
+++ eslint.config.js
@@ -1,23 +1,39 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
+import eslint from '@eslint/js';
+import eslintConfigPrettier from 'eslint-config-prettier/flat';
+import react from 'eslint-plugin-react';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
 
 export default tseslint.config(
-  { ignores: ['dist'] },
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
-    files: ['**/*.{ts,tsx}'],
+    ignores: ['dist', 'node_modules'],
+  },
+  {
+    extends: [eslint.configs.recommended, ...tseslint.configs.recommended],
+    files: ['**/*.{js,jsx,mjs,cjs,ts,tsx}'],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+    },
+    settings: {
+      react: {
+        version: 'detect',
+      },
     },
     plugins: {
+      react,
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
     },
     rules: {
+      ...react.configs.flat.recommended.rules,
       ...reactHooks.configs.recommended.rules,
       'react-refresh/only-export-components': [
         'warn',
@@ -25,4 +41,5 @@ export default tseslint.config(
       ],
     },
   },
-)
+  eslintConfigPrettier,
+);
